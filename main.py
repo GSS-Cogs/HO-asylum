bc51c6c72ce3a31052f7a58b9e42b09b472ee74b
RedWalters <jonathan.j.walters@Gmail.com>
2019-11-08 14:51:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import glob
 py_files = [i for i in glob.glob('*.{}'.format('py'))]
 
 for i in py_files:
-    file = "'" + i + "'"
+    file = "'./" + i + "'"
     if 'main' in file:
         pass
-    %run file
+    %run $file
